dd2f4ac8ac2e4592441283f4c319d05b437f9514
Red Siwon Choi <red@zoyi.co>
2015-07-31 13:44:51 +0900

    Init project.
    - Apply Gradle.
    - Add Socket.IO java library.
    - Make demo Main class.

---
+++ src/com/zoyi/Main.java
@@ -0,0 +1,46 @@
+package com.zoyi;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
+public class Main {
+
+  public static void main(String[] args) throws URISyntaxException {
+    // write your code here
+    final Socket socket;
+
+    IO.Options opts = new IO.Options();
+    opts.forceNew = true;
+    opts.reconnection = false;
+    opts.query = "user_email=" + "" + "&user_token=" + "" + "&shop_id=608";
+    opts.path = "/sensor.io";
+
+    socket = IO.socket("https://station5.walkinishgts.com/api/shops/signals", opts);
+
+    socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+      @Override
+      public void call(Object... args) {
+        System.out.println("connected");
+      }
+    }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+      @Override
+      public void call(Object... args) {
+        System.out.println("disconnected");
+      }
+    }).on("signal", new Emitter.Listener() {
+
+      @Override
+      public void call(Object... args) {
+        JSONObject obj = (JSONObject)args[0];
+        System.out.println(obj);
+      }
+
+    });
+
+    socket.connect();
+  }
+}
